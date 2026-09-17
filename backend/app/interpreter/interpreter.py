02ea41b4baf91325d49a276516149b7b9aa07508
isaias <isaiamoraisdelima@gmail.com>
2026-09-17 01:42:18 -0300

    ajustatan esqueleto de front e class interprete

--- backend/app/interpreter/interpreter.py
+++ backend/app/interpreter/interpreter.py
@@ -1,34 +1,37 @@
+from sqlalchemy.ext.asyncio import result
+
 from backend.app.schemas.command_scherma import CommandSchema
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta, time
+from datetime import timedelta
+import re
+import unicodedata
 
 
 class Interpreter:
 
 
-    def parse_reminder_datetime(text: str):
+    def parse_reminder_datetime(self,text: str):
 
-        if "amanhã às 15:00" in text:
-            tomorrow = datetime.now() + timedelta(days=1)
+        dia = self.identificar_dia(text)
+        horario = self.procurar_horario(text)
+        horario = self.transformar_horario(horario)
+        data_e_hora = self.montar_data_hora(comando=dia, hora=horario)
 
-            return tomorrow.replace(
-                hour=15,
-                minute=0,
-                second=0,
-                microsecond=0
-            )
+        return data_e_hora
 
-        return None
+
+
 
 
 
     def interpret(self, text: str):
 
-        text = text.lower().strip()
+        text = self.texto_formatado(text)
 
         if text.startswith("criar lembrete "):
-            remind_at = Interpreter.parse_reminder_datetime(text)
+            remind_at = self.parse_reminder_datetime(text)
 
-            title = text.removeprefix("criar lembrete ").strip()
+            title = self.extrair_titulos_lembretes(text)
 
             return CommandSchema(
                 type="lembrete",
@@ -60,3 +63,77 @@ class Interpreter:
                 )
 
         return None
+
+    def procurar_horario(self,texto: str):
+
+        resultado = re.search(r"\d\d:\d\d", texto)
+
+        if not resultado:
+            return None
+        return resultado.group()
+
+    def transformar_horario(self,texto: str):
+
+        if not texto:
+            return None
+
+        partes = texto.split(':')
+
+        hora = int(partes[0])
+        minutos = int(partes[1])
+
+        hora_completa = time(hour=hora, minute=minutos)
+
+        return hora_completa
+
+    @staticmethod
+    def texto_formatado(texto: str):
+        texto_sem_acento = unicodedata.normalize('NFKD', texto).encode('ascii', 'ignore').decode('utf-8')
+        texto_limpo = texto_sem_acento.lower()
+        return texto_limpo
+
+
+    def identificar_dia(self,texto: str):
+
+        texto_limpo = Interpreter.texto_formatado(texto)
+
+        if 'depois' in texto_limpo and 'amanha' in texto_limpo:
+            return 2
+
+        if 'amanha' in texto_limpo:
+            return 1
+
+        if 'hoje' in texto_limpo:
+            return 0
+
+        return None
+
+    def montar_data_hora(self,comando: int | None, hora: time):
+
+        hoje = datetime.now()
+
+        if not comando:
+            comando = 0
+
+        data = hoje + timedelta(days=comando)
+
+        data = data.replace(hour=hora.hour, minute=hora.minute, second=0, microsecond=0)
+
+        return data
+
+
+    def extrair_titulos_lembretes(self,texto: str):
+
+        texto = Interpreter.texto_formatado(texto)
+
+        texto = texto.removeprefix("criar lembrete ").strip()
+
+        resultado = re.search(r"hoje|amanha|depois de amanhã", texto)
+
+        if not resultado:
+            return None
+
+        return texto[:resultado.start()]
+
+
+
--- backend/app/teste_command.py
+++ backend/app/teste_command.py
@@ -1,76 +1,23 @@
 from datetime import datetime, timedelta, time
 import re
 from datetime import timedelta
-
+from backend.app.interpreter.interpreter import Interpreter
 import unicodedata
 
-def procurar_horario(texto:str):
 
-    resultado = re.search(r"\d\d:\d\d" , texto)
+def extrair_titulos_lembretes(texto: str):
+
+    texto = Interpreter.texto_formatado(texto)
+
+    texto = texto.removeprefix("criar lembrete ").strip()
+
+    resultado = re.search(r"hoje|amanha|depois de amanhã", texto)
 
     if not resultado:
-       return None
-    return resultado.group()
-
-
-
-
-def transformar_horario(texto:str):
-
-    if not texto:
         return None
 
-    partes = texto.split(':')
-
-    hora = int(partes[0])
-    minutos = int(partes[1])
-
-
-    hora_completa = time(hour=hora, minute=minutos)
-
-    return hora_completa
+    return texto[:resultado.start()]
 
 
 
-
-def identificar_dia(texto:str):
-
-    texto_sem_acento = unicodedata.normalize('NFKD', texto).encode('ascii', 'ignore').decode('utf-8')
-
-    texto_limpo = texto_sem_acento.lower()
-
-    if 'depois' in texto_limpo and 'amanha' in texto_limpo:
-        return 2
-
-    if 'amanha' in texto_limpo:
-        return 1
-
-    if 'hoje' in texto_limpo:
-        return 0
-
-    return None
-
-
-def montar_data_hora(comando:int|None,hora:time):
-
-    hoje = datetime.now()
-
-    if not comando:
-        comando = 0
-
-    data = hoje + timedelta(days=comando)
-
-    data = data.replace(hour=hora.hour, minute=hora.minute)
-
-    return data
-
-texto = 'faco isso amanha as 14:01'
-
-hora_str = procurar_horario(texto)
-
-hora = transformar_horario(hora_str)
-
-comando = identificar_dia(texto)
-
-
-print(montar_data_hora(comando,hora))
+print(extrair_titulos_lembretes('isaias amanha 14:00'))
